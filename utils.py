beb96a79da937b0d83dfb0bb49bc09721dee7d09
Gustavo Sada <gustavogssilva@gmail.com>
2019-03-20 23:10:49 -0300

    initial stage (first commit)

---
+++ utils.py
@@ -0,0 +1,44 @@
+import sys
+import numpy as np
+from PIL import Image
+
+# images = map(Image.open, ['Test1.jpg', 'Test2.jpg', 'Test3.jpg'])
+# widths, heights = zip(*(i.size for i in images))
+#
+# total_width = sum(widths)
+# max_height = max(heights)
+#
+# new_im = Image.new('RGB', (total_width, max_height))
+#
+# x_offset = 0
+# for im in images:
+#   new_im.paste(im, (x_offset,0))
+#   x_offset += im.size[0]
+#
+# new_im.save('test.jpg')
+
+#coded for van gogh dataset (37 images)
+def saveComparisonImage(original, decoded):
+
+    original = np.uint8(255*original)
+    decoded = np.uint8(255*decoded)
+
+    imY = np.shape(original)[1]
+    imX = np.shape(original)[2]
+    height = 8*imX
+    width = 8*imY
+
+    newImage = Image.new('RGB', (width, height))
+
+    line = 0
+    x_offset = 0
+
+    for i in range(len(original)):
+        if i % 4 == 0 and i != 0:
+            line += 1
+            x_offset = 0
+        newImage.paste(Image.fromarray(original[i]), (x_offset,line*imY))
+        newImage.paste(Image.fromarray(decoded[i]), (x_offset+imX,line*imY))
+        x_offset += 2*imX
+
+    newImage.save('output.jpg')
---
+++ vae-preprocess.py
@@ -0,0 +1,47 @@
+import os
+import argparse
+from keras.preprocessing import image as image_utils
+from PIL import Image
+from PIL import ImageFilter
+import matplotlib.pyplot as plt
+import numpy as np
+
+#----------- CONSTANTS ------------
+
+IMAGE_SIZE = (300, 200)
+
+#----------------------------------
+
+
+#----------- PARSE PARAMETERS ------------
+parser = argparse.ArgumentParser()
+parser.add_argument('--data', '-d', help='path of the dataset')
+args = parser.parse_args()
+dataPath = args.data
+
+if not dataPath:
+    # print('error: specify dataset with --data arg')
+    # exit()
+    dataPath = 'VG_data'
+#------------------------------------------
+
+
+dataset = []
+imageList = os.listdir(dataPath)
+print("Found {0} images in {1} dataset".format(len(imageList), dataPath))
+
+idx = 1
+for imageName in imageList:
+    imagePath = os.path.join(dataPath, imageName)
+    image = image_utils.load_img(imagePath).resize(IMAGE_SIZE, Image.ANTIALIAS)
+    imageData = np.array(image.getdata()).reshape(IMAGE_SIZE[0], IMAGE_SIZE[1], 3)
+    imageData = imageData.astype('float32')/255
+    # imageData = imageData.astype('float32')
+    dataset.append(imageData)
+    print(str(idx)+'- Loaded image '+imageName)
+    idx+=1
+
+dataset = np.array(dataset)
+print('Dataset Shape:', dataset.shape)
+np.save(dataPath, dataset)
+print('Succesfully created {0}.npy'.format(dataPath))
