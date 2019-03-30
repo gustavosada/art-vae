8be6d6777e654c0227299b04e14d9f6ab6de7053
Gustavo Sada <gustavogssilva@gmail.com>
2019-03-30 17:52:28 -0300

    initial defs/imports and load dataset

--- vae.py
+++ vae.py
@@ -6,19 +6,20 @@ import math
 import model
 from parameters import *
 
-tf.reset_default_graph()
-# sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# CRIAR UM MAIN E DEFINIR ESSE ARQUIVO DE VAE COMO UMA CLASSE, ASSIM DA PRA USAR O TREINO
+# DE DIVERSAS FORMAS, COMPARAR DIFERENTES PARAMETROS DE ENTRADA
+# MAS ANTES AJEITAR A PARTE DE INPUT PARA PODER USAR OUTROS DATASETS COMO O DE MODA
 
-raw_dataset = np.load("VG_data.npy")
-dataset_size = len(raw_dataset)
 
-if batch_size > dataset_size:
-    batch_size = dataset_size
+to_categorical = tf.keras.utils.to_categorical
+losses = tf.keras.losses
+optimizers = tf.keras.optimizers
 
-X_in = tf.placeholder(dtype=tf.float32, shape=[None, image_size[0], image_size[1], 3], name='X')
-Y    = tf.placeholder(dtype=tf.float32, shape=[None, image_size[0], image_size[1], 3], name='Y')
-Y_flat = tf.reshape(Y, shape=[-1, image_size[0] * image_size[1] * 3])
-keep_prob = tf.placeholder(dtype=tf.float32, shape=(), name='keep_prob')
+K = tf.keras.backend
+K.set_epsilon(1e-05)
+
+dataset = np.load("fashion_data.npy")
+dataset_size = len(dataset)
 
 
 sampled, mn, sd = model.encoder(X_in, keep_prob)
@@ -32,11 +33,8 @@ optimizer = tf.train.AdamOptimizer(0.0005).minimize(loss)
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-
-dataset = tf.data.Dataset.from_tensor_slices(raw_dataset)
-dataset = dataset.batch(batch_size)
-iterator = dataset.make_initializable_iterator()
-next_batch = iterator.get_next()
+print("PAUSA 3")
+input()
 
 for i in range(epochs):
         print("----- EPOCH #%s -----" % (i+1))
