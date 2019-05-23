e7aa4456b4b2d6cd21d3b0f13f8dafac3cecd9bb
Gustavo Sada <gustavogssilva@gmail.com>
2019-05-22 21:03:50 -0300

    fix wrong path bug

--- vae.py
+++ vae.py
@@ -34,19 +34,14 @@ vae, encoder, decoder, beta = model.build()
 if load:
     filename = load
     vae.load_weights(os.path.join(experiment_dir, filename+".h5"))
-    encoder.load_weights(os.path.join(experiment_dir, filename+"-enc.h5"))
-    decoder.load_weights(os.path.join(experiment_dir, filename+"-dec.h5"))
+    experiment_dir = os.path.join(experiment_dir, time())
+    utils.createDir(experiment_dir)
 
 
 # save model info
 utils.saveModelDescription(encoder, decoder, experiment_dir)
 
 # set training
-def saveSubModels(epoch, log={}):
-    if epoch % 80 == 0 and epoch != 0:
-        encoder.save_weights(os.path.join(experiment_dir, "checkpoint-{epoch:02d}-{loss:02f}-enc.h5".format(epoch=epoch, loss=log['loss'])))
-        decoder.save_weights(os.path.join(experiment_dir, "checkpoint-{epoch:02d}-{loss:02f}-dec.h5".format(epoch=epoch, loss=log['loss'])))
-
 def warmup(epoch):
     threshold_epoch = 5
     if epoch > threshold_epoch:
@@ -59,19 +54,25 @@ def warmup(epoch):
     print("beta:", value)
     K.set_value(beta, value)
 
+def saveSubModels(epoch, log={}):
+    frequency = 80
+    if epoch % frequency == 0 and epoch != 0:
+        encoder.save_weights(os.path.join(experiment_dir, "checkpoint-{epoch:02d}-{loss:02f}-enc.h5".format(epoch=epoch, loss=log['loss'])))
+        decoder.save_weights(os.path.join(experiment_dir, "checkpoint-{epoch:02d}-{loss:02f}-dec.h5".format(epoch=epoch, loss=log['loss'])))
 
 def saveCurrentSampleCB(epoch):
-    if epoch % 10 == 0 and epoch != 0:
+    frequency = 1
+    if epoch % frequency == 0 and epoch != 0:
         utils.saveCurrentSample(encoder, decoder, dataset[0], os.path.join(experiment_dir, "sample-{}.jpg".format(epoch)))
 
-saveCB = tf.keras.callbacks.ModelCheckpoint(os.path.join(experiment_dir, "checkpoint-{epoch:02d}-{loss:02f}.h5"), monitor='loss', verbose=0, save_best_only=True, save_weights_only=True, mode='auto', period=80)
+saveFrequency = 80
+saveCB = tf.keras.callbacks.ModelCheckpoint(os.path.join(experiment_dir, "checkpoint-{epoch:02d}-{loss:02f}.h5"), period=saveFrequency, monitor='loss', verbose=0, save_best_only=True, save_weights_only=True, mode='auto')
 saveSubModelsCB = tf.keras.callbacks.LambdaCallback(on_epoch_end=lambda epoch, log: saveSubModels(epoch, log))
 warmupCB = tf.keras.callbacks.LambdaCallback(on_epoch_begin=lambda epoch, log: warmup(epoch))
 saveImageCB = tf.keras.callbacks.LambdaCallback(on_epoch_end=lambda epoch, log: saveCurrentSampleCB(epoch))
 tensorboard = TensorBoard(log_dir="logs/{}".format(experiment))
 earlystop = tf.keras.callbacks.EarlyStopping(monitor='loss', min_delta=0.00001, patience=5, verbose=10, mode='auto')
-# callbacks_list = [earlystop, tensorboard, warmupCB]
-# callbacks_list = [tensorboard, warmupCB, saveImageCB, saveSubModelsCB]
+
 callbacks_list = [tensorboard, saveImageCB, saveSubModelsCB]
 
 if save_flag:
@@ -91,17 +92,7 @@ utils.saveComparisonImages(encoder, decoder, dataset, experiment_dir)
 utils.saveInterpolationImages(encoder, decoder, dataset, experiment_dir)
 
 
-# save model weights -> DEVERIA TROCAR PRA SALVAR OS PROPRIOS MODELOS (FAZ MAIS SENTIDO)
-del dataset
-gc.collect()
-
 if save_flag:
+    del dataset
+    gc.collect()
     vae.save_weights(os.path.join(experiment_dir, "vae.h5"))
-    del vae
-    gc.collect()
-    encoder.save_weights(os.path.join(experiment_dir, "enc.h5"))
-    del encoder
-    gc.collect()
-    decoder.save_weights(os.path.join(experiment_dir, "dec.h5"))
-    del decoder
-    gc.collect()
